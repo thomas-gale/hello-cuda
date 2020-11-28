ec615a6d5bb67b08881efe04119202f50a4c3f22
Thomas Gale <gale.thomasjames@gmail.com>
2020-11-28 21:29:12 +0000

    Hello cuda running :)

--- src/Main.cu
+++ src/Main.cu
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <math.h>
 
-// function to add the elements of two arrays
-void add(int n, float* x, float* y) {
+// Kernel function to add the elements of two arrays
+__global__ void add(int n, float* x, float* y) {
     for (int i = 0; i < n; i++)
         y[i] = x[i] + y[i];
 }
 
-// Following: https://developer.nvidia.com/blog/even-easier-introduction-cuda/
 int main(void) {
-    int N = 1 << 20; // 1M elements
+    int N = 1 << 20;
+    float *x, *y;
 
-    float* x = new float[N];
-    float* y = new float[N];
+    // Allocate Unified Memory – accessible from CPU or GPU
+    cudaMallocManaged(&x, N * sizeof(float));
+    cudaMallocManaged(&y, N * sizeof(float));
 
     // initialize x and y arrays on the host
     for (int i = 0; i < N; i++) {
@@ -20,8 +21,11 @@ int main(void) {
         y[i] = 2.0f;
     }
 
-    // Run kernel on 1M elements on the CPU
-    add(N, x, y);
+    // Run kernel on 1M elements on the GPU
+    add<<<1, 1>>>(N, x, y);
+
+    // Wait for GPU to finish before accessing on host
+    cudaDeviceSynchronize();
 
     // Check for errors (all values should be 3.0f)
     float maxError = 0.0f;
@@ -30,8 +34,8 @@ int main(void) {
     std::cout << "Max error: " << maxError << std::endl;
 
     // Free memory
-    delete[] x;
-    delete[] y;
+    cudaFree(x);
+    cudaFree(y);
 
     return 0;
 }
