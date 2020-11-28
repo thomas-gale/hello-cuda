c27757b6d1377a9287d8f5a3a27330a7435ac020
Thomas Gale <gale.thomasjames@gmail.com>
2020-11-28 21:41:47 +0000

    Running on threads.

--- src/Main.cu
+++ src/Main.cu
@@ -3,7 +3,9 @@
 
 // Kernel function to add the elements of two arrays
 __global__ void add(int n, float* x, float* y) {
-    for (int i = 0; i < n; i++)
+    int index = threadIdx.x;
+    int stride = blockDim.x;
+    for (int i = index; i < n; i += stride)
         y[i] = x[i] + y[i];
 }
 
@@ -22,7 +24,7 @@ int main(void) {
     }
 
     // Run kernel on 1M elements on the GPU
-    add<<<1, 1>>>(N, x, y);
+    add<<<1, 256>>>(N, x, y);
 
     // Wait for GPU to finish before accessing on host
     cudaDeviceSynchronize();
